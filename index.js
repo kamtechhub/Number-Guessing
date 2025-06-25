94517f4015f9c92665c15cd3d93c0337bf456c8e
Kamoru AbdulRasak Oyinlola <rkamoru1@gmail.com>
2025-06-25 11:29:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let min = 1;
+let max = 15;
+let generatedNum = Math.floor(Math.random() * max + min)
+let textContent1 = document.getElementById("textContent1");
+let attempt = 0;
+let guess;
+let running = true
+
+while(running){
+    guess = window.prompt("Guess a number from 1 to 15");
+    guess = Number(guess);
+    if(isNaN(guess)){
+        window.alert("Enter a valid number")
+    }else if(guess < min || guess > max){
+           window.alert("Enter a valid number")
+    } else {
+        attempt++
+        if (guess < generatedNum) {
+            window.alert("TRY AGAIN!, TOO LOW")
+        }else if(guess > generatedNum) {
+            window.alert("TRY AGAIN!, TOO HIGH")
+        } else {
+           textContent1.textContent =  `Correct! The answer was ${generatedNum} it took ${attempt} attempts`;
+            running = false
+        }
+    }
+}
+
